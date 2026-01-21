ba558909688344efd2266a068e21945a30b88344
Leviox428 <150800327+Leviox428@users.noreply.github.com>
2026-01-21 19:31:38 +0100

    Update routing.ts

--- developer-portfolio-website/src/i18n/routing.ts
+++ developer-portfolio-website/src/i18n/routing.ts
@@ -3,7 +3,6 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
     // A list of all locales that are supported
     locales: ['sk', 'en'],
-    
     // Used when no locale matches
     defaultLocale: 'sk',
     localeDetection: true
