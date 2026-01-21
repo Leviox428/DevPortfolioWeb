7d16ae4779744d1f3794ebdbb390fdc030b3a41f
Leviox428 <150800327+Leviox428@users.noreply.github.com>
2026-01-21 19:28:21 +0100

    fixed minor bug

--- developer-portfolio-website/src/app/[locale]/page.tsx
+++ developer-portfolio-website/src/app/[locale]/page.tsx
@@ -1,13 +1,13 @@
 import EntryPoint from "@/src/components/EntryPoint";
+import SolarSystemScene from "@/src/components/solarSystemModel/SolarSystemScene";
 import { generateToken } from "@/src/models/serverModels/pageModel";
 
 export default async function Home() {
   const token = await generateToken();
 
   return  (
-    <EntryPoint
-      token={token}
-    >     
+    <EntryPoint token={token}>
+      <SolarSystemScene />
     </EntryPoint>
   );
 }
