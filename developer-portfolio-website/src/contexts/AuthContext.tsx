cc7f8772fa102300051d55859e1ba1fe56d0d2a6
Leviox428 <150800327+Leviox428@users.noreply.github.com>
2026-01-21 19:31:32 +0100

    reworked auth context

--- developer-portfolio-website/src/contexts/AuthContext.tsx
+++ developer-portfolio-website/src/contexts/AuthContext.tsx
@@ -1,36 +1,47 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import { getSession } from "../models/reviewSectionModel";
-import { useFormState } from "react-hook-form";
+"use client"
 
+import { createContext, useContext, useState, useEffect } from "react";
+import { getSession } from "../models/authModel";
+import { useToken } from "./TokenContext";
 
 type AuthContextType = {
     uid: string | null;
     isAuth: boolean;
-    authToken: string,
+    fullName: string;
+    email: string;
     setUid: (uid: string | null) => void;
     setIsAuth: (auth: boolean) => void;
-    setAuthToken: (authToken: string) => void;
-};
+    setFullName: (fullName: string) => void;
+    setEmail: (email: string) => void;
+}; 
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
+export function AuthProvider({ children, isAdmin }: { children: React.ReactNode, isAdmin: boolean }) {
     const [uid, setUid] = useState<string | null>(null);
     const [isAuth, setIsAuth] = useState(false);
-    const [authToken, setAuthToken] = useState<string>("");
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const token = useToken();
 
     useEffect(() => {
-        async function load() {
-            const data = await getSession();
-            setUid(data.uid);
-            setAuthToken(data.authToken);
-            setIsAuth(Boolean(data.uid));
+        async function loadSession() {
+            const data = await getSession(token);
+            
+            if (data && data.isAuth) {
+                setUid(data.uid);
+                setIsAuth(data.isAuth);
+                setFullName(data.fullName);
+                setEmail(data.email)
+            }
         }
-        load();
+        if (!isAdmin) {
+            loadSession();
+        }
     }, []);
 
     return (
-        <AuthContext.Provider value={{ uid, isAuth, authToken, setUid, setIsAuth, setAuthToken }}>
+        <AuthContext.Provider value={{ uid, isAuth, fullName, email, setUid, setIsAuth, setFullName, setEmail }}>
             {children}
         </AuthContext.Provider>
     );
