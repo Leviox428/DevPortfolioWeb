337e91d07d7acea4355dc17565fa064ef9588486
Leviox428 <150800327+Leviox428@users.noreply.github.com>
2026-01-21 19:32:34 +0100

    Update reviewsSectionTypes.ts

--- developer-portfolio-website/src/models/types/reviewsSectionTypes.ts
+++ developer-portfolio-website/src/models/types/reviewsSectionTypes.ts
@@ -1,7 +1,7 @@
 import { FieldInfo } from "./common";
 
 export interface ReviewType {
-    author: string;
+    fullName: string;
     content: string;
     stars: number;
     source: string;
@@ -11,9 +11,10 @@ export interface ReviewType {
 export const authCardFieldInfoDict: Record<string, FieldInfo> = {
     email: { min: 10, max: 50},
     password: { min: 8, max: 50 },
+    fullName: { min: 4, max: 50}
 }
 
 export const reviewEditorFieldInfoDict: Record<string, FieldInfo> = {
-    author: { min: 6, max: 30 },
     content: { min: 10, max: 200}
 }
+
